dc762e4e71a5ea1582c8a013d73aa3bac4da21a4
Furkan <furkan.ozkeroglu@hotmail.com>
2025-07-05 21:59:28 +0300

    slider_done

---
+++ src/app/font/page.tsx
@@ -0,0 +1,104 @@
+
+
+export default function FontPage() {
+  return (
+    <>
+    <div className="font-avenir-black">
+      FONT-AVENİR-BLACK
+    </div>
+    <div className="font-avenir-black-oblique">
+      FONT-AVENİR-BLACK-OBLİQUE
+    </div>
+    <div className="font-avenir-book">
+      FONT-AVENİR-BOOK
+    </div>
+    <div className="font-avenir-book-oblique">
+      FONT-AVENİR-BOOK-OBLİQUE
+    </div>
+    <div className="font-avenir-heavy">
+      FONT-AVENİR-HEAVY
+    </div>
+    <div className="font-avenir-heavy-oblique">
+      FONT-AVENİR-HEAVY-OBLİQUE
+    </div>
+    <div className="font-avenir-light">
+      FONT-AVENİR-LİGHT
+    </div>
+    <div className="font-avenir-light-oblique">
+      FONT-AVENİR-LİGHT-OBLİQUE
+    </div>
+    <div className="font-avenir-medium">
+      FONT-AVENİR-MEDİUM
+    </div>
+      <div className="font-avenir-medium-oblique">
+      FONT-AVENİR-MEDİUM-OBLİQUE
+    </div>
+    <div className="font-avenir-oblique">
+      FONT-AVENİR-OBLİQUE
+    </div>
+    <div className="font-avenir-roman">
+      FONT-AVENİR-ROMAN
+    </div>
+    <div>
+      -----------------------------------NORMAL YAZI BİÇİMİ BUDUR-------------------------------------------
+    </div>
+    <div className="font-montserrat-black">
+      FONT-MONTSERRAT-BLACK
+    </div>
+    <div className="font-montserrat-black-italic">
+      FONT-MONTSERRAT-BLACK-İTALİC
+    </div>
+    <div className="font-montserrat-bold">
+      FONT-MONTSERRAT-BOLD
+    </div>
+    <div className="font-montserrat-bold-ttf">
+      FONT-MONTSERRAT-BOLD-TTF
+    </div>
+    <div className="font-montserrat-bold-italic">
+      FONT-MONTSERRAT-BOLD-İTALİC
+    </div>
+    <div className="font-montserrat-extra-bold">
+      FONT-MONTSERRAT-EXTRA-BOLD
+    </div>
+    <div className="font-montserrat-extra-bold-italic">
+      FONT-MONTSERRAT-EXTRA-BOLD-İTALİC
+    </div>
+    <div className="font-montserrat-extra-light">
+      FONT-MONTSERRAT-EXTRA-LİGHT
+    </div>
+    <div className="font-montserrat-extra-light-italic">
+      FONT-MONTSERRAT-EXTRA-LİGHT-İTALİC
+    </div>
+    <div className="font-montserrat-italic">
+      FONT-MONTSERRAT-İTALİC
+    </div>
+    <div className="font-montserrat-light">
+      FONT-MONTSERRAT-LİGHT
+    </div>
+    <div className="font-montserrat-light-italic">
+      FONT-MONTSERRAT-LİGHT-İTALİC
+    </div>
+    <div className="font-montserrat-medium">
+      FONT-MONTSERRAT-MEDİUM
+    </div>
+    <div className="font-montserrat-medium-italic">
+      FONT-MONTSERRAT-MEDİUM-İTALİC
+    </div>
+    <div className="font-montserrat-regular">
+      FONT-MONTSERRAT-REGULAR
+    </div>
+    <div className="font-montserrat-semibold">
+      FONT-MONTSERRAT-SEMİBOLD
+    </div>
+    <div className="font-montserrat-smeibold-italic">
+      FONT-MONTSERRAT-SEMİBOLD-İTALİC
+    </div>
+    <div className="font-montserrat-thin">
+      FONT-MONTSERRAT-THİN
+    </div>
+    <div className="font-montserrat-thin-italic">
+      FONT-MONTSERRAT-THİN-İTALİC
+    </div>
+    </>
+  );
+}
